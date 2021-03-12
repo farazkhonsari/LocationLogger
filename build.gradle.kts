8dfae80e37855bc2266ff04bc4fc555b4e70ad70
faraz khonsari <FarazKhonsari@gmail.com>
2021-03-12 14:59:58 +0330

    Faraz:

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,27 +7,30 @@
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
-
+    id("maven-publish")
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
 }
 
+group = "com.github.jitpack"
+version = "0.0.2"
 repositories {
     // Use jcenter for resolving dependencies.
     // You can declare any Maven/Ivy/file repository here.
     jcenter()
+    google()
+    maven { url = uri("https://jitpack.io") }
+
 }
 
 dependencies {
     // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
